a1ab4d91d0dc03bb5d7a140c6a4c0fbfbfbac525
Mustafa <mustafaarifin88@admin.sd.belajar.id>
2026-03-11 11:47:44 +0700

    Auto Save

---
+++ app/Models/ProfilSekolahModel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ProfilSekolahModel extends Model
+{
+    protected $table            = 'profil_sekolah';
+    protected $primaryKey       = 'id';
+    protected $returnType       = 'object';
+    protected $allowedFields    = [
+        'nama_sekolah', 'alamat_lengkap', 'desa', 'nama_kepsek', 'nip_kepsek', 'ttd_kepsek'
+    ];
+}
