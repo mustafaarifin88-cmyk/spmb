26a3710d1c3565cfcca75d975266aa8ac6b7c203
Mustafa <mustafaarifin88@admin.sd.belajar.id>
2026-03-11 17:24:41 +0700

    Auto Save

--- spmb.sql
+++ spmb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 10, 2026 at 06:56 PM
+-- Generation Time: Mar 11, 2026 at 11:16 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -32,6 +32,13 @@ CREATE TABLE `agama` (
   `nama_agama` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `agama`
+--
+
+INSERT INTO `agama` (`id`, `nama_agama`) VALUES
+(1, 'Islam');
+
 -- --------------------------------------------------------
 
 --
@@ -45,6 +52,14 @@ CREATE TABLE `berkas_pendaftaran` (
   `file_path` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `berkas_pendaftaran`
+--
+
+INSERT INTO `berkas_pendaftaran` (`id`, `id_pendaftaran`, `id_persyaratan`, `file_path`) VALUES
+(1, 1, 1, '1773223392_71549023fc8761ddbbb2.png'),
+(2, 1, 2, '1773223392_3a36b77a40931ca4ce1a.png');
+
 -- --------------------------------------------------------
 
 --
@@ -57,6 +72,13 @@ CREATE TABLE `latar_belakang` (
   `is_active` tinyint(1) DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `latar_belakang`
+--
+
+INSERT INTO `latar_belakang` (`id`, `gambar`, `is_active`) VALUES
+(1, '1773217278_6ccdaf5d5c62a9fd1bc3.png', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -67,6 +89,13 @@ CREATE TABLE `pekerjaan` (
   `id` int(11) NOT NULL,
   `nama_pekerjaan` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pekerjaan`
+--
+
+INSERT INTO `pekerjaan` (`id`, `nama_pekerjaan`) VALUES
+(1, 'PNS');
 
 -- --------------------------------------------------------
 
@@ -120,6 +149,13 @@ CREATE TABLE `pendaftaran` (
   `updated_at` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `pendaftaran`
+--
+
+INSERT INTO `pendaftaran` (`id`, `id_user`, `no_kk`, `nik_siswa`, `nama_peserta_didik`, `nama_panggilan`, `jenis_kelamin`, `tempat_lahir`, `tanggal_lahir`, `id_agama`, `kewarganegaraan`, `anak_ke`, `jumlah_saudara_kandung`, `jumlah_saudara_angkat`, `bahasa_sehari_hari`, `berat_badan`, `tinggi_badan`, `alamat`, `no_telp`, `tempat_tinggal`, `nama_ayah`, `nama_ibu`, `nik_ayah`, `nik_ibu`, `pendidikan_ayah`, `pendidikan_ibu`, `penghasilan_ayah`, `penghasilan_ibu`, `id_pekerjaan_ayah`, `id_pekerjaan_ibu`, `nama_wali`, `pendidikan_wali`, `hubungan_wali`, `id_pekerjaan_wali`, `masuk_sebagai`, `asal_peserta_didik`, `nama_tk`, `tahun_nomor_ijazah`, `ttd_ortu`, `status_pendaftaran`, `alasan_tolak`, `created_at`, `updated_at`) VALUES
+(1, 2, '123456', '121837', 'Mus J', 'Mus', 'Laki-laki', 'Jakarta', '2026-03-10', 1, 'WNI', 2, 2, 0, 'Indonesia ', 1, 10, 'Jakarta Barat ', '0822888777222', 'Orang Tua', 'Ayah', 'Ibu', '128822726', '9281716', 'S3', 'S2', '> 2.000.000', '> 2.000.000', 1, 1, '', '', '', NULL, 'Murid Baru', 'Sekolah Herry Potter ', 'Kanak', '81817162625252', '69b13ddfa98bd.png', 'Diterima', NULL, '2026-03-11 17:03:11', '2026-03-11 17:05:33');
+
 -- --------------------------------------------------------
 
 --
@@ -130,6 +166,14 @@ CREATE TABLE `persyaratan` (
   `id` int(11) NOT NULL,
   `nama_persyaratan` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `persyaratan`
+--
+
+INSERT INTO `persyaratan` (`id`, `nama_persyaratan`) VALUES
+(1, 'Foto Copy Ijazah Legis'),
+(2, 'Foto Copy KK Legis');
 
 -- --------------------------------------------------------
 
@@ -144,15 +188,17 @@ CREATE TABLE `profil_sekolah` (
   `desa` varchar(100) NOT NULL,
   `nama_kepsek` varchar(100) NOT NULL,
   `nip_kepsek` varchar(50) NOT NULL,
-  `ttd_kepsek` varchar(255) DEFAULT NULL
+  `ttd_kepsek` varchar(255) DEFAULT NULL,
+  `logo_pemda` varchar(255) DEFAULT NULL,
+  `logo_sekolah` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `profil_sekolah`
 --
 
-INSERT INTO `profil_sekolah` (`id`, `nama_sekolah`, `alamat_lengkap`, `desa`, `nama_kepsek`, `nip_kepsek`, `ttd_kepsek`) VALUES
-(1, 'SMA Negeri 1 Contoh', 'Jl. Pendidikan No. 1', 'Desa Contoh', 'Budi Santoso, S.Pd., M.Pd.', '19800101 200501 1 001', NULL);
+INSERT INTO `profil_sekolah` (`id`, `nama_sekolah`, `alamat_lengkap`, `desa`, `nama_kepsek`, `nip_kepsek`, `ttd_kepsek`, `logo_pemda`, `logo_sekolah`) VALUES
+(1, 'SMA Negeri 1 Contoh', 'Jl. Pendidikan No. 1', 'Desa Contoh', 'Budi Santoso, S.Pd., M.Pd.', '19800101 200501 1 001', NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -176,7 +222,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `nama_lengkap`, `username`, `password`, `foto_profil`, `level`, `created_at`, `updated_at`) VALUES
-(1, 'Administrator', 'admin', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'default.png', 'admin', '2026-03-11 00:50:28', '2026-03-11 00:50:28');
+(1, 'Administrator', 'admin', '$2y$10$57qSO2MRSClHSBp5.EDyTepRDkkgNiAHUKmUq5YnLeDVTjYd0g4AS', 'default.png', 'admin', '2026-03-11 00:50:28', '2026-03-11 14:14:13'),
+(2, 'Mustafa Arifin', 'siswa', '$2y$10$ee0ujDdIbWTox7WCPgPqceXr6HqCieb7/ZtQ6ZmtEE5mrgVVJ5EgW', '1773214998_ab01670a3e6cc715e50c.png', 'siswa', '2026-03-11 14:38:26', '2026-03-11 14:43:18');
 
 --
 -- Indexes for dumped tables
@@ -246,37 +293,37 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `agama`
 --
 ALTER TABLE `agama`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `berkas_pendaftaran`
 --
 ALTER TABLE `berkas_pendaftaran`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `latar_belakang`
 --
 ALTER TABLE `latar_belakang`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `pekerjaan`
 --
 ALTER TABLE `pekerjaan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `pendaftaran`
 --
 ALTER TABLE `pendaftaran`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `persyaratan`
 --
 ALTER TABLE `persyaratan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `profil_sekolah`
@@ -288,7 +335,7 @@ ALTER TABLE `profil_sekolah`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
