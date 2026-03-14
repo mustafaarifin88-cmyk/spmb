4750e5c239949c51b24f4bd51759f5be51fc6cef
Mustafa <mustafaarifin88@admin.sd.belajar.id>
2026-03-15 00:08:56 +0700

    Auto Save

--- app/Controllers/Siswa/Dashboard.php
+++ app/Controllers/Siswa/Dashboard.php
@@ -4,16 +4,33 @@ namespace App\Controllers\Siswa;
 
 use App\Controllers\BaseController;
 use App\Models\PendaftaranModel;
+use App\Models\PengaturanModel;
+use App\Models\BerkasFisikModel;
+use App\Models\PersyaratanModel;
+use App\Models\BerkasPendaftaranModel;
 
 class Dashboard extends BaseController
 {
     public function index()
     {
         $pendaftaranModel = new PendaftaranModel();
-        $id_user = session()->get('id');
+        $pengaturanModel = new PengaturanModel();
         
-        $data['pendaftaran'] = $pendaftaranModel->where('id_user', $id_user)->first();
+        $data['pendaftaran'] = $pendaftaranModel->where('id_user', session()->get('id'))->first();
+        $data['pengaturan'] = $pengaturanModel->first();
         
+        if ($data['pendaftaran'] && $data['pendaftaran']->status_pendaftaran == 'Diterima') {
+            $berkasFisikModel = new BerkasFisikModel();
+            $data['berkas_fisik'] = $berkasFisikModel->findAll();
+        }
+
+        if ($data['pendaftaran'] && $data['pendaftaran']->status_pendaftaran == 'Perbaikan') {
+            $persyaratanModel = new PersyaratanModel();
+            $berkasPendaftaranModel = new BerkasPendaftaranModel();
+            $data['persyaratan'] = $persyaratanModel->findAll();
+            $data['berkas_lama'] = $berkasPendaftaranModel->where('id_pendaftaran', $data['pendaftaran']->id)->findAll();
+        }
+
         return view('siswa/dashboard', $data);
     }
 }
